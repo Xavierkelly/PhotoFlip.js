1b16b85f43e7f4f8f6e744690418da5afe110118
Xavier Kelly <Xavierkelly72@gmail.com>
2016-06-16 07:54:17 -0500

    Improved transitions smoothness of images

--- src/PhotoFlip.js
+++ src/PhotoFlip.js
@@ -10,14 +10,15 @@
         var settings = $.extend({
             interval            : '3000', 
             backgroundSize     :  'cover',
-            repeat              : 'no-repeat'
+            repeat              : 'no-repeat',
+            transitionTime      :   '0.5s'
         }, options);
 
         container = this;
         imagePosition = 0;
 
         //Set up the css
-        this.css('background-repeat', settings.repeat).css('background-size', settings.backgroundSize).css('background-image', images[0]);
+        this.css('background-repeat', settings.repeat).css('background-size', settings.backgroundSize).css('background-image', images[0]).css('transition',  settings.transitionTime);
 
         //Start the interval to change the background
         setInterval(function(){
